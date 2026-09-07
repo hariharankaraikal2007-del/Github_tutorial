63fcd8d8b4045b8c7f2356e857d8b6aa6c759b93
udhayakumar <udhayaak2830@gmail.com>
2026-09-07 15:31:23 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+for(let fruit of fruits){
+    console.log(fruit); 
+}
+let data ={
+    name: "Udhay",
+    age: 20,
+}
+
+for(let key in data){
+    console.log(key );
+}
+
+
